21c7014cf8ed127e7c4d7245ac0181b614ab3828
Dylan Authentic <dylan@authentic.tech>
2021-01-07 13:38:57 -0500

    Code Corrections

--- january7th/largeIntegerAddition.js
+++ january7th/largeIntegerAddition.js
@@ -2,13 +2,13 @@
 function bigAddition(str1, str2) {
     let sum = '';
 
-    let topNumberLength = str1.length;
-    let bottomNumberLength = str2.length;
+    let topNumber = str1;
+    let bottomNumber = str2;
 
-    if(bottomNumberLength > topNumberLength) {
-        let temp = topNumberLength;
-        topNumberLength = bottomNumberLength;
-        bottomNumberLength = temp;
+    if(bottomNumber.length > topNumber.length) {
+        let temp = topNumber;
+        topNumber = bottomNumber;
+        bottomNumber = temp;
     }
 
     let carry = 0;
@@ -16,17 +16,23 @@ function bigAddition(str1, str2) {
     let b;
     let temp; 
     let singleDigitSum;
-    for(let i = 0; i < topNumberLength; i++) {
-        a = parseInt(str1.length - 1 - i);
-        b = parseInt(str2.length - 1 - i);
-        b = b ? b : 0;
+    for(let i = 0; i < topNumber.length; i++) {
+        a = parseInt(topNumber.charAt(topNumber.length - 1 - i));
+        b = parseInt(bottomNumber.charAt(bottomNumber.length - 1 - i));
+        b = (b) ? b : 0;
         temp  = (carry + a + b).toString();
-        singleDigitSum = parseInt(temp.length - 1);
+        singleDigitSum = temp.charAt(temp.length - 1);
         carry = parseInt(temp.charAt(0));
-        carry = carry ? carry : 0;
+        carry = (carry) ? carry : 0;
         
-        sum = (i === topNumberLength - 1) ? temp + sum : singleDigitSum + sum; 
+        sum = (i === topNumber.length - 1) ? temp + sum : singleDigitSum + sum; 
     }
 
     return sum;
 }
+
+var value1 = '90071992547409916324883284778932'
+var value2 = '90071992547409912354653283'; 
+
+bigAddition(value1, value2); // 189992179709403064034802729432265
+bigAddition("2750", "3750") // 6500
